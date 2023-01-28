5c2e31a30202267098a7c0d1fa85b00c13a61bf0
Jan Pöschko <jan@ketryx.com>
2023-01-28 14:54:10 +0100

    Rename input parameters

--- src/input.ts
+++ src/input.ts
@@ -12,23 +12,23 @@ export type ActionInput = {
 };
 
 export function readActionInput(): ActionInput {
-  const ketryxUrl = core.getInput('ketryxUrl') || 'https://app.ketryx.com';
+  const ketryxUrl = core.getInput('ketryx-url') || 'https://app.ketryx.com';
 
   const project = core.getInput('project');
   if (!project) {
     throw new Error('Missing input project');
   }
 
-  const apiKey = core.getInput('apiKey');
+  const apiKey = core.getInput('api-key');
   if (!apiKey) {
-    throw new Error('Missing input apiKey');
+    throw new Error('Missing input api-key');
   }
 
   const version = core.getInput('version');
-  const commitSha = core.getInput('commitSha') || process.env.GITHUB_SHA;
-  const buildName = core.getInput('buildName');
+  const commitSha = core.getInput('commit-sha') || process.env.GITHUB_SHA;
+  const buildName = core.getInput('build-name');
 
-  const testCucumberPath = core.getInput('testCucumberPath');
+  const testCucumberPath = core.getInput('test-cucumber-path');
 
   const log = core.getInput('log');
 
