c57f5a1681b94546bde3da744393055f61ee3e4f
Jan Pöschko <jan@ketryx.com>
2023-05-16 12:44:22 +0200

    Add null check

--- src/util.ts
+++ src/util.ts
@@ -2,5 +2,5 @@ export function hasProperty<T, K extends string>(
   o: T,
   k: K
 ): o is T & Record<K, unknown> {
-  return typeof o === 'object' && k in o;
+  return typeof o === 'object' && o !== null && k in o;
 }
